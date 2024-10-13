70472141ae4a0bab08ba05ed2222dad6db6719c6
siddhanth-tiwari <pratimatiwari8299@gmail.com>
2024-10-13 14:04:12 +0530

    setup requirements update

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguages',
+    version= '0.0.0',
+    author= 'siddhanth',
+    author_email= 'siddhanthtiwari7709@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
