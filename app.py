11eb3504c04bc876a0da73aea395b00d2f8e36d9
siddhanth-tiwari <pratimatiwari8299@gmail.com>
2024-10-13 16:19:50 +0530

    update Exception and app

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 from signLanguage.logger import logging
+import sys
+from signLanguage.exception import SignException
 
-logging.info("Welcome to signLanguage project")
+# logging.info("Welcome to signLanguage project") # logging at info level
+
+try:
+    a = 7 / "9"
+    
+except Exception as e:
+    raise SignException(e, sys) from e
